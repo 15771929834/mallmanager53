4d3f0d6cca3fbed505f4190d21d6d8907a4f9734
bbzhang <bbzhang@yuansuan.cn>
2019-10-21 17:20:01 +0800

    新建登录分支

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+// import Login from '../components/login/login.vue'
+// @ 自动找src文件夹
+import Login from '@/components/login/login'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/'
+      name: 'login',
+      path: '/login',
+      component: Login
     }
   ]
 })
